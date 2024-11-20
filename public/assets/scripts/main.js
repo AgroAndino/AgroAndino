d67c5f524213bdd810bb91287f214f8e40a9b6b1
St4rLght05 <144155675+St4rLght05@users.noreply.github.com>
2024-11-20 17:40:24 -0500

    Update main.js

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,1 +1,30 @@
+//Verificación de formulario
+document
+  .getElementById("InscripSec")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    const form = document.querySelector('form[name="frm"]');
+    form.addEventListener("submit", (event) => {
+      const fname = form.elements["fname"].value;
+      const flname = form.elements["flname"].value;
+      const femail = form.elements["femail"].value;
+      const fphone = form.elements["fphone"].value;
+
+      if (!fname || !flname || !femail || !fphone) {
+        event.preventDefault();
+        alert("Please, complete all the required fields");
+      } else if (!validateEmail(femail)) {
+        event.preventDefault();
+        alert("Please, insert a valid email");
+      } else {
+        alert("Thank you for helping us grow!");
+      }
+    });
+
+    //crear función validateEmail
+    function validateEmail(femail) {
+      const re = /^[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z]/;
+      return re.test(String(femail).toLowerCase());
+    }
+  });
